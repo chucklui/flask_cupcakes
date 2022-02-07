27eb39a0196b3058072a68f29870a8a9c84e1498
Lidan-Huang <huangld0314@gmail.com>
2022-02-07 12:13:49 -0800

    finished part 1, cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 """Flask app for Cupcakes"""
+from models import Cupcake, connect_db, db
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,28 @@
 """Models for Cupcake app."""
+from unittest.mock import DEFAULT
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = 'https://tinyurl.com/demo-cupcake'
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
